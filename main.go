40bfa1923050fcbe274d85ab2dfe2cceb99353b1
Andre Costa <mr.luizandre@gmail.com>
2022-09-26 14:07:54 -0300

    Slices

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// DECLARAÇÃO DE VARIÁVEIS
 	// Go infere o tipo de dado quando definimos o valor na declaração
 	var conferenceName = "Conferência Go" // alternativa => conferenceName := 50, e não funciona pra declarar o tipo explicitamente
 	const referenceTickets = 50           // não há alternativa para constantes
@@ -12,23 +14,26 @@ func main() {
 	fmt.Printf("Há o total de %v e há %v vagas restantes.\n", referenceTickets, remainingTickets)
 	fmt.Println("Obtenha aqui seus passes para participação no", "evento.")
 
-	// Data types
-	// string
-	// int / uint / int8 / int16 ...
-	// float32 / float64
-
-	// Tem que definir o tamanho e não pode misturar diferentes tipos
-	// var bookings = [50]string{"Andre", "Luiz"}
-	// bookings[0] = "Andre"
-	// bookings[1] = "Luiz"
-
-	var bookings [50]string
-
 	// Quando não definimos o valor na declaração um tipo dado deve ser informado
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
+
+	// := operador de short variable declaration
+	// Exemplo: nome := "Andre" ao invés de var nome strng = "Andre"
+
+	// DATA TYPES
+	// string
+	// int / uint / int8 / int16 ...
+	// float32 / float64
+
+	// ARRAYS
+	// Tem que definir o tamanho e não pode misturar diferentes tipos
+	// var bookings = [50]string{"Andre", "Luiz"}
+	// bookings[0] = "Andre"
+	// bookings[1] = "Luiz"
+	// var bookings [50]string
 
 	fmt.Print("Informe o primeiro nome: ")
 	fmt.Scan(&firstName)
@@ -39,14 +44,16 @@ func main() {
 	fmt.Print("Informe a quantidade de ingressos: ")
 	fmt.Scan(&userTickets)
 
-	bookings[0] = firstName + lastName
+	// SLICES
+	// slice is an abstraction of array
+	// slices are more flexible and powerful: variable length or get an sub-array of its own
+	// slices are also index-based and have a size, but is resized when needed
+	var bookings []string
+	// bookings := []string{"Andre", "Luiz"}
 
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 	remainingTickets = remainingTickets - userTickets
-
-	fmt.Printf("Array inteiro: %v\n", bookings)
-	fmt.Printf("Array primeiro item: %v\n", bookings[0])
-	fmt.Printf("Array tipo: %T\n", bookings[0])
-	fmt.Printf("Array tamanho: %v\n", len(bookings))
 
 	fmt.Printf(
 		"Obrigado %v %v por reservar %v ingressos. A confirmação será enviada para o e-mail %v.\n",
@@ -55,6 +62,7 @@ func main() {
 		userTickets,
 		email,
 	)
-
 	fmt.Printf("%v ingressos restantes para %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("Esses são todos os ingressos: %v\n", bookings)
 }
