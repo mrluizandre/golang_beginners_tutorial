16dc9b7d8beee4a6daac7445a806d310b3838213
Andre Costa <andre.costa@rdstation.com>
2022-10-03 16:05:43 -0300

    add some package level variables

--- main.go
+++ main.go
@@ -5,38 +5,16 @@ import (
 	"strings"
 )
 
+const conferenceTickets = 50
+
+var conferenceName = "Conferência Go"
+var remainingTickets uint = 50
+var bookings []string
+
 func main() {
-	// DECLARAÇÃO DE VARIÁVEIS
-	// Go infere o tipo de dado quando definimos o valor na declaração
-	var conferenceName = "Conferência Go" // alternativa => conferenceName := 50, e não funciona pra declarar o tipo explicitamente
-	const conferenceTickets = 50          // não há alternativa para constantes
-	var remainingTickets uint = 50
 
-	greetUsers(conferenceName, conferenceTickets, remainingTickets)
+	greetUsers()
 
-	// := operador de short variable declaration
-	// Exemplo: nome := "Andre" ao invés de var nome strng = "Andre"
-
-	// DATA TYPES
-	// string
-	// int / uint / int8 / int16 ...
-	// float32 / float64
-
-	// ARRAYS
-	// Tem que definir o tamanho e não pode misturar diferentes tipos
-	// var bookings = [50]string{"Andre", "Luiz"}
-	// bookings[0] = "Andre"
-	// bookings[1] = "Luiz"
-	// var bookings [50]string
-
-	// SLICES
-	// slice is an abstraction of array
-	// slices are more flexible and powerful: variable length or get an sub-array of its own
-	// slices are also index-based and have a size, but is resized when needed
-	var bookings []string
-	// bookings := []string{"Andre", "Luiz"}
-
-	// for remainingTickets > 0 {
 	for {
 		firstName, lastName, email, userTickets := getUserInput()
 
@@ -45,16 +23,14 @@ func main() {
 			lastName,
 			email,
 			userTickets,
-			remainingTickets,
 		)
 
 		if isValidName && isValidEmail && isValidTicketsNumber {
-			bookTicket(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
+			bookTicket(userTickets, firstName, lastName, email)
 
-			firstNames := getFirstNames(bookings)
+			firstNames := getFirstNames()
 			fmt.Printf("Os primeiros nomes dos ingressos: %v\n", firstNames)
 
-			// var noTickerRemaining bool = remainingTickets == 0
 			if remainingTickets == 0 {
 				fmt.Println("Os ingressos acabaram. Volte novamente ano que vem!")
 				break
@@ -73,13 +49,13 @@ func main() {
 	}
 }
 
-func greetUsers(confName string, conferenceTickets int, remainingTickets uint) {
-	fmt.Printf("Bem-vindo a apricação de venda de ingressas da %v!\n", confName)
+func greetUsers() {
+	fmt.Printf("Bem-vindo a apricação de venda de ingressas da %v!\n", conferenceName)
 	fmt.Printf("Há o total de %v e há %v vagas restantes.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Obtenha aqui seus passes para participação no", "evento.")
 }
 
-func getFirstNames(bookings []string) []string {
+func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
 		var names = strings.Fields(booking)
@@ -93,7 +69,6 @@ func validateUserInput(
 	lastName string,
 	email string,
 	userTickets uint,
-	remainingTickets uint,
 ) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
@@ -121,13 +96,10 @@ func getUserInput() (string, string, string, uint) {
 }
 
 func bookTicket(
-	remainingTickets uint,
 	userTickets uint,
-	bookings []string,
 	firstName string,
 	lastName string,
 	email string,
-	conferenceName string,
 ) {
 	bookings = append(bookings, firstName+" "+lastName)
 	remainingTickets = remainingTickets - userTickets
