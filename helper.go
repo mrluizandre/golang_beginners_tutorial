926aa0a3bbcd8d3536e7908f8f11c0e7c351c856
Andre Costa <andre.costa@rdstation.com>
2022-10-04 08:47:59 -0300

    Add 2nd function to package

---
+++ helper.go
@@ -0,0 +1,16 @@
+package main
+
+import "strings"
+
+func validateUserInput(
+	firstName string,
+	lastName string,
+	email string,
+	userTickets uint,
+) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketsNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketsNumber
+}
